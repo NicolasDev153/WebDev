4832abc383c125261401fc7b767a10b2258eb157
NicolasDev153 <nikaberidze27@gmail.com>
2021-12-30 00:07:14 +0400

    Code for scrolling

--- CV/main.js
+++ CV/main.js
@@ -1,4 +1,5 @@
-/*==================== SHOW MENU ====================*/
+/*SHOW MENU*/
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -13,7 +14,8 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/*REMOVE MENU MOBILE */
+
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
@@ -22,3 +24,25 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*SCROLL SECTIONS ACTIVE LINK */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
